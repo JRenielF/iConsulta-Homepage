b4c8152eb239693fb18dd06d331bee1252fa56e0
JRenielF <jeraldreniel@gmail.com>
2024-04-30 11:17:17 +0800

    css

--- iconsulta/src/components/App.jsx
+++ iconsulta/src/components/App.jsx
@@ -6,9 +6,11 @@ import Testimonial from '/src/components/Testimonial';
 import Services from '/src/components/Services';
 import { Footer } from '/src/components/Footer';
 import Feature from '/src/components/Feature';
+import Navbar from './Navbar';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Navbar />
     <Home />
     <Services />
     <Feature />
